c926b16755a5bb330c0590d4158da3a238616ce0
kingreevice <129922512+kingreevice@users.noreply.github.com>
2024-12-02 16:21:55 +0800

    Update kpkr.js

--- js/kpkr.js
+++ js/kpkr.js
@@ -21,7 +21,7 @@ const UA = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML,
 const appConfig = {
     ver: 1,
     title: '桃子影视',
-    site: 'https://www.taozi008.com/index.html',
+    site: 'https://www.taozi008.com',
     // 定義分類
     tabs: [
         // name 為分類名，ext 可以傳入任意參數由 getCards 接收

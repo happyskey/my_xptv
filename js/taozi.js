13bd52e50075c91f0141dbb15ec9e27ee72f841f
kingreevice <129922512+kingreevice@users.noreply.github.com>
2024-12-05 08:55:04 +0800

    Update taozi.js

--- js/taozi.js
+++ js/taozi.js
@@ -178,7 +178,7 @@ async function getPlayinfo(ext) {
     })
 
 
-  //  data = argsify(data)
+   data = argsify(data)
         
     const url =  'https://v4.fentvoss.com/sdv4/202412/04/PLw0AyTRFs22/video/index.m3u8'//ext.url
 

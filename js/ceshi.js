145c32941e985c88fd80a542a12bb64f83637d58
kingreevice <129922512+kingreevice@users.noreply.github.com>
2024-12-06 10:20:14 +0800

    Update ceshi.js

--- js/ceshi.js
+++ js/ceshi.js
@@ -1,31 +1,10 @@
 const cheerio = createCheerio()
-// const CryptoJS = createCryptoJS()
-// const JSEncrypt = loadJSEncrypt()
-/*
-以上是可以調用的第三方庫，使用方法自行查閱文檔
-內置方法有:
-$print: 等同於 console.log
-$fetch: http client，可發送 get 及 post 請求
-    get: $fetch.get(url,options)
-    post: $fetch.post(url,postData,options)
-argsify, jsonify: 等同於 JSON 的 parse 及 stringify
-$html: 內置的 html 解析方法，建議用 cheerio 替代
-$cache: 可將數據存入緩存
-    set: $cache.set(key, value)
-    get: $cache.get(key)
-*/
-
-
-// 預先定義請求使用的 user-agent
 const UA = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36'
-
 const appConfig = {
     ver: 1,
     title: '樱花动漫',
     site: 'https://yhdm.one',
-    // 定義分類
     tabs: [
-        // name 為分類名，ext 可以傳入任意參數由 getCards 接收
         {
             name: '日本动漫',
             ext: {
@@ -52,63 +31,31 @@ const appConfig = {
         
     ],
 }
-
-// 進入源時調用，ver,title,site,tabs 為必須項
 async function getConfig() {
     return jsonify(appConfig)
 }
-
-
-
-
-
-// 取得分類的影片列表，ext 為 tabs 定義的 ext 加上頁碼(page)
 async function getCards(ext) {
-    // 將 JSON 字符串轉為 JS 對象
     ext = argsify(ext)
-    // 定義一個空的卡片數組
     let cards = []
-    // 從 ext 中解構賦值取出頁數及分類 id，等同於:
-    // let page = ext.page
-    // let id = ext.id
     let { page = 1, id } = ext
-
-    // 定義請求的 URL
     const url =appConfig.site + `/list/?country=${id}&page=${page}` 
-    // 使用內置的 http client 發起請求獲取 html
     const { data } = await $fetch.get(url, {
         headers: {
             'User-Agent': UA,
         },
     })
-
-    // 用 cheerio 解析 html
     const $ = cheerio.load(data)
-
-    // 用 css 選擇器選出影片列表
     const videos = $('ul.list-unstyled li')
-    // 遍歷所有影片
     videos.each((_, e) => {
         const href = $(e).find('a').attr('href')
         const title = $(e).find('h6').text()
         const cover =appConfig.site + $(e).find('img').attr('data-original')
         //const remarks = $(e).find('.note > span').text()
-
-
-       
-
-
-
-
-
-        
-        // 將每個影片加入 cards 數組中
         cards.push({
             vod_id: href,
             vod_name: title,
             vod_pic: cover,
             //vod_remarks: remarks, // 海報右上角的子標題
-            // ext 會傳給 getTracks
             ext: {
                 url: `${appConfig.site}${href}`,
             },
@@ -120,10 +67,14 @@ async function getCards(ext) {
     })
 }
 
-/*
 
 
-//获取单个播放列表
+
+
+
+
+
+
 async function getTracks(ext) {
     
     ext = argsify(ext)
@@ -140,10 +91,10 @@ async function getTracks(ext) {
 
     const $ = cheerio.load(data)
 
-    const playlist = $('#eps-ul .play-btn')
+    const playlist = $('a')
     playlist.each((_, e) => {
-        const name = $(e).find('a').text()
-        const ShareUrl =appConfig.site + $(e).find('a').attr('href')
+        const name = $(e).attr('href')
+        const ShareUrl =appConfig.site + $(e).attr('title')
 
         
 
@@ -159,7 +110,6 @@ async function getTracks(ext) {
     })
 
     return jsonify({
-        // list 返回一個數組，用於區分不同線路(參考 AGE 動漫及 girigiri 動漫)，但功能未實現目前只會讀取第一項
         list: [
             {
                 title: '默认分组',
@@ -169,22 +119,12 @@ async function getTracks(ext) {
     })
 }
 
-    
 
 
 async function getPlayinfo(ext) {
-    
     ext = argsify(ext)
-
-    
     const idMatch = ext.url.match(/[?&]line_id=([^&]*)/)[1];
     let get_url = `https://www.taozi008.com/openapi/playline/${idMatch}`
-
-
-
-
-
-
    const {data} = await $fetch.get(get_url, {
         headers: {
             'User-Agent': UA,
@@ -192,20 +132,11 @@ async function getPlayinfo(ext) {
     })
 
          if (data) {
-             //目前这个JSON.parse方法可靠 ，argsify 不适用于解析
             const result = JSON.parse(data)
             let playUrl = result.info.file        
             return jsonify({ urls: [playUrl] })
          }
-
-
-
-    
 }
-
-
-
-//搜索
 async function search(ext) {
     ext = argsify(ext)
     let cards = []

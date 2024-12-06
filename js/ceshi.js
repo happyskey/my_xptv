82fadce92f6e193f28a6edf2cb10a21c82755879
kingreevice <129922512+kingreevice@users.noreply.github.com>
2024-12-06 15:51:17 +0800

    Update ceshi.js

--- js/ceshi.js
+++ js/ceshi.js
@@ -94,7 +94,7 @@ async function getTracks(ext) {
         const ShareUrl =appConfig.site + $(e).attr('href')  
 
 
-
+/*
         
         const { new_data } = await $fetch.get(ShareUrl, {
              'User-Agent': UA,
@@ -128,10 +128,10 @@ async function getTracks(ext) {
     }
 
         
+*/
 
 
-
-        /*
+    
         tracks.push({
             name:name.trim(),
             pan: '',
@@ -167,12 +167,15 @@ async function getTracks(ext) {
 
 
 
-    */
+   
 }
 
- return jsonify({ list: groups })
+// return jsonify({ list: groups })
 
-                  }
+            //      }
+
+
+
 
 
 

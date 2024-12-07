1e9dc5f3bf57ed371027aa81cf7d94d9a9e84571
kingreevice <129922512+kingreevice@users.noreply.github.com>
2024-12-07 15:52:01 +0800

    Update ceshi.js

--- js/ceshi.js
+++ js/ceshi.js
@@ -92,21 +92,20 @@ async function getTracks(ext) {
 
     const $ = cheerio.load(data)
     const playlist = $('.ep-panel.mb-3 a')
-    playlist.each((_, e) => {
-       
-        let name = $(e).attr('title')
-        const ShareUrl =appConfig.site + $(e).attr('href')  
+  //  playlist.each((_, e) => {
+       for(let i =0; i <playlist.length; i++){
+           
+        let title = $(e).attr('title')
+        const href = $(e).attr('href').match(/\/vod-play\/([^\/]+\/[^\.]+)/)[1]
+        const ShareUrl = `https://yhdm.one/_get_plays/${href}`
 
 
-
-
-        
         let group = {
-          title: name,
-          tracks: [],
-    }
-        
+                title: title.trim(),
+                tracks: [],
+            };
 
+    
         const new_data  = await $fetch.get(ShareUrl, {
                      'User-Agent': UA,
                           });
@@ -114,35 +113,24 @@ async function getTracks(ext) {
   
         const json = argsify(new_data)
         const playlists = json.video_plays
-
-
-
-        //const playlists = [ { "play_data": "https://bfikuncdn.com/20241205/uWfZfPZ1/index.m3u8", "src_site": "ikzy" }, { "play_data": "https://hd.ijycnd.com/play/lejpXKWb/index.m3u8", "src_site": "jyzy" }, { "play_data": "https://svipsvip.ffzy-online5.com/20241205/35797_0126af62/index.m3u8", "src_site": "ffzy" }, { "play_data": "https://v.cdnlz22.com/20241205/9243_64206af9/index.m3u8", "src_site": "lzzy" }, { "play_data": "https://v5.tlkqc.com/wjv5/202412/05/iJic8fALXb77/video/index.m3u8", "src_site": "wjzy2" }, { "play_data": "https://cdn.wlcdn99.com:777/3ed0e23f/index.m3u8", "src_site": "wlzy2" }, { "play_data": "https://v11.dious.cc/20241205/ibTpuTaV/index.m3u8", "src_site": "tkzy2" }, { "play_data": "https://v4.qrssv.com/202412/05/CsZZ37hQ1d22/video/index.m3u8", "src_site": "snzy" }, { "play_data": "https://play.modujx16.com/20241205/z8WCjLPY/index.m3u8", "src_site": "mdzy" }, { "play_data": "https://vv.jisuzyv.com/play/nelrZglb/index.m3u8", "src_site": "jszy" } ]
-        playlists.forEach( each => {
-        
-
-        let line = each.src_site
-        let path = each.play_data
+  playlists.forEach( child => {
         
         group.tracks.push({
-          name:  line,
+          name:  child.src_site,
           pan: '',
           ext: {
-            url: path
+            url: child.play_data
           }
         })
-
-
    
 
     })
 
+if (group.tracks.length > 0) {
+            groups.push(group)
+          }
 
-      groups.push(group)
-    
-
-
-        })    
+    }    
 
 
 return jsonify({ list: groups })

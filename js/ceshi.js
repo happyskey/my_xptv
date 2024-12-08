c4ca62dae1eedf4ef4632b49896f223d4c361c1d
kingreevice <129922512+kingreevice@users.noreply.github.com>
2024-12-08 12:18:19 +0800

    Update ceshi.js

--- js/ceshi.js
+++ js/ceshi.js
@@ -83,7 +83,7 @@ async function getTracks(ext) {
     
     let url = ext.url
     
-    
+    let tacks = []
 
     const { data } = await $fetch.get(url, {
         headers: {
@@ -95,59 +95,42 @@ async function getTracks(ext) {
     
 
     const $ = cheerio.load(data)
-    const playlist =$('ul.row.list-unstyled.gutters-1 li a') //$('.ep-panel.mb-3 a')
+    const playlist =$('ul.row.list-unstyled.gutters-1 li a') 
  
        for(let i =0; i <playlist.length; i++){
            
         let title = $(e).attr('title')
         const href = $(e).attr('href').match(/\/vod-play\/([^\/]+\/[^\.]+)/)[1]
-        const ShareUrl = `https://yhdm.one/_get_plays/${href}`
+        const ShareUrl = appConfig.site`/vod-play/${href}.html`
 
 
-        let group = {
-                title: title.trim(),
-                tracks: [],
-            };
 
-    
-        const new_data  = await $fetch.get(ShareUrl, {
-                     'User-Agent': UA,
-                          });
-        new_data = { "video_plays": [ { "play_data": "https://v.gsuus.com/play/lejpXGlb/index.m3u8", "src_site": "gszy" }, { "play_data": "https://hd.ijycnd.com/play/nelrZKrb/index.m3u8", "src_site": "jyzy" }, { "play_data": "https://hn.bfvvs.com/play/negmRB3b/index.m3u8", "src_site": "hnzy" }, { "play_data": "https://play.xluuss.com/play/mbkqYJEe/index.m3u8", "src_site": "xlzy" }, { "play_data": "https://bfikuncdn.com/20241206/5z7Ht4nK/index.m3u8", "src_site": "ikzy" }, { "play_data": "https://svipsvip.ffzy-online5.com/20241206/35807_e83a34b2/index.m3u8", "src_site": "ffzy" }, { "play_data": "https://v.cdnlz22.com/20241206/9254_08559580/index.m3u8", "src_site": "lzzy" }, { "play_data": "https://v6.tlkqc.com/wjv6/202412/06/bpV4EFgEf478/video/index.m3u8", "src_site": "wjzy2" }, { "play_data": "https://v11.dious.cc/20241206/FxNOvAi2/index.m3u8", "src_site": "tkzy2" }, { "play_data": "https://v6.qrssv.com/202412/06/Kavu6MZvir24/video/index.m3u8", "src_site": "snzy" }, { "play_data": "https://vv.jisuzyv.com/play/penw2MEa/index.m3u8", "src_site": "jszy" }, { "play_data": "https://play.modujx16.com/20241206/BWXIO9F6/index.m3u8", "src_site": "mdzy" } ]}
-        const json = JSON.parse(new_data)
-        const playlists = json.video_plays
+     
         playlists.forEach( child => {
         
-            group.tracks.push({
-              name:  child.src_site,
+           tracks.push({
+              name:  ShareUrl,
               pan: '',
               ext: {
-                url: child.play_data
+                url: ShareUrl,
               }
             })
    
 
     })
 
-if (group.tracks.length > 0) {
-            groups.push(group)
-          }
-
-    }    
 
 
-return jsonify({ list: groups })
+    return jsonify({
+        list: [
+            {
+                title: '默认分组',
+                tracks,
+            },
+        ],
+    })
       
 
-
-
-
-
-
-
-
-
- //   return jsonify({"list":[{"title":"第01集","tracks":[{"name":"lzzy","pan":"","ext":{"url":"https://v.lzcdn23.com/20241108/7572_ee376b18/index.m3u8"}}]},{"title":"预告","tracks":[{"name":"tkzy2","pan":"","ext":{"url":"https://v10.dious.cc/20240329/8sSjB4Uz/index.m3u8"}}]}]})
 }
 
 

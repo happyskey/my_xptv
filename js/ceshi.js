2d66156157843afbe68055c1b5452e360df80bf0
kingreevice <129922512+kingreevice@users.noreply.github.com>
2024-12-09 07:42:10 +0800

    Update ceshi.js

--- js/ceshi.js
+++ js/ceshi.js
@@ -180,79 +180,45 @@ async function getPlayinfo(ext) {
 
 
 
-/*
-//
-async function getTracks(ext) {
-    ext = argsify(ext);
-    let groups = [];
-    let url = ext.url;
+async function search(ext) {
+    ext = argsify(ext)
+    let cards = []
 
-    // 获取页面数据
+    let text = encodeURIComponent(ext.text)
+    let page = ext.page || 1
+    let url = `${appConfig.site}/search?q=%E5%90%8D=${text}`//https://yhdm.one/search?q=%E5%90%8D
+
     const { data } = await $fetch.get(url, {
         headers: {
             'User-Agent': UA,
-            'Referer': 'https://yhdm.one/',
-            'Origin': 'https://yhdm.one',
         },
-    });
+    })
 
-    // 使用 cheerio 解析 HTML
-    const $ = cheerio.load(data);
-    const playlist = $('.ep-panel.mb-3 a');
+    const $ = cheerio.load(data)
 
-    console.log("Playlist length:", playlist.length);  // 打印 playlist 长度，确认是否匹配到元素
+    const videos = $('#search_list li')
+    videos.each((_, e) => {
+        const link = $(e).find('a');
+        const href = link.attr('href')
+        const title =link.attr('title')
+        const img = $(e).find('img');
+        const cover =img.attr('src'); 
 
-    if (playlist.length === 0) {
-        return jsonify({ list: groups });
-    }
+        cards.push({
+            vod_id: href,
+            vod_name: title,
+            vod_pic: cover,
+            vod_remarks: '',
 
-    // 遍历 playlist 中的每个元素
-    for (const e of playlist) {
-        let name = $(e).attr('title');
-        const href = $(e).attr('href')//.match(/\/vod-play\/([\d\/]+)\.html/);
-
-        console.log("Href matched:", href);  // 打印 href 的匹配结果
-
-        if (!href) continue; // 如果 href 为空，跳过
-
-        const getID = appConfig.site + href[1];  // 获取ID
-        let new_url = 'https://yhdm.one/_get_plays/' + getID;
-
-        // 使用静态数据测试
-        const playlists = [
-            { "play_data": "https://hd.ijycnd.com/play/9b6589Na/index.m3u8", "src_site": "jyzy" },
-            { "play_data": "https://hn.bfvvs.com/play/Le351wpb/index.m3u8", "src_site": "hnzy" },
-            { "play_data": "https://play.xluuss.com/play/7e55yLXe/index.m3u8", "src_site": "xlzy" }
-        ];
-
-        // 为每个播放源添加到 tracks 中
-        let group = {
-            title: name, // 集数
-            tracks: [],
-        };
-
-        for (const d of playlists) {
-            group.tracks.push({
-                name: d.src_site,
-                pan: '',
-                ext: {
-                    url: d.play_data,
-                },
-            });
-        }
-
-        if (group.tracks.length > 0) {
-            groups.push(group);
-        }
-    }
-
-    // 返回最终的数据
-    return jsonify({ list: groups });
+            ext: {
+                url: `${appConfig.site}${href}`,
+            },
+        })
+    })
+    return jsonify({
+        list: cards,
+    })
 }
-
-
-
-*/
 
 
 

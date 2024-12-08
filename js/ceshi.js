3ab4ec5b47ca12a43a5433ae65b8685c65d2d9f5
kingreevice <129922512+kingreevice@users.noreply.github.com>
2024-12-08 13:16:58 +0800

    Update ceshi.js

--- js/ceshi.js
+++ js/ceshi.js
@@ -52,7 +52,12 @@ async function getCards(ext) {
         const href = $(e).find('a').attr('href') //视频连接后缀
         const title = $(e).find('h6').text()    //标题
         const cover =appConfig.site + $(e).find('img').attr('data-original') //图片
-
+        const new_url = appConfig.site + href
+          const  new_data  = await $fetch.get(new_url, {
+        headers: {
+            'User-Agent': UA,
+        },
+    })
 
         
         //const remarks = $(e).find('.note > span').text() //右上更新

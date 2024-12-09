591e2159a3b5b022b40f8b2054e392d4b6147bd1
kingreevice <129922512+kingreevice@users.noreply.github.com>
2024-12-09 20:13:14 +0800

    Update ceshi.js

--- js/ceshi.js
+++ js/ceshi.js
@@ -3,30 +3,60 @@ const cheerio = createCheerio()
 const UA = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36'
 const appConfig = {
     ver: 1,
-    title: '樱花动漫',
-    site: 'https://yhdm.one',
+    title: '美剧网',
+    site: 'https://www.j00j.com/',
     tabs: [
         {
-            name: '日本动漫',
+            name: '欧美剧',
             ext: {
-                id: 'jp',
+                id: 20,
             },
         },
         {
-            name: '国产动漫',
+            name: '新马泰剧',
             ext: {
-                id: 'cn',
+                id: 21,
             },
         },
         {
-            name: '欧美动漫',
+            name: '韩剧',
             ext: {
-                id: 'us',
+                id: 22,
             },
         },{
-            name: '其他的',
+            name: '日剧',
             ext: {
-                id: 'other',
+                id: 23,
+            },
+        },{
+            name: '台剧',
+            ext: {
+                id: 25,
+            },
+        },{
+            name: '在线电影',
+            ext: {
+                id: 24,
+            },
+        },{
+            name: '在线综艺',
+            ext: {
+                id: 36,
+            },
+        },{
+            name: '在线动漫',
+            ext: {
+                id: 43,
+            },
+        },{
+            name: '在线预告',
+            ext: {
+                id: 48,
+            },
+        },{
+            name: '在线短剧',
+            ext: {
+                id: 49,
             },
         }
         

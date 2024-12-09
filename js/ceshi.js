a44694861a1a907a825413835decb29bc6fd6eed
kingreevice <129922512+kingreevice@users.noreply.github.com>
2024-12-09 09:13:01 +0800

    Update ceshi.js

--- js/ceshi.js
+++ js/ceshi.js
@@ -3,30 +3,30 @@ const cheerio = createCheerio()
 const UA = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36'
 const appConfig = {
     ver: 1,
-    title: '樱花动漫',
-    site: 'https://yhdm.one',
+    title: '黑猫TV|昊',
+    site: 'https://heimaotv.vip',
     tabs: [
         {
-            name: '日漫',
+            name: '电影',
             ext: {
-                id: 'jp',
+                id: '/vodshow-31.html',
             },
         },
         {
-            name: '国产动漫',
+            name: '电视剧',
             ext: {
-                id: 'cn',
+                id: '/vodshow-49.html',
             },
         },
         {
-            name: '欧美动漫',
+            name: '综艺',
             ext: {
-                id: 'us',
+                id: '/vodshow-66.html',
             },
         },{
-            name: '其他的',
+            name: '抖音短视频',
             ext: {
-                id: 'other',
+                id: '/vodshow-117.html',
             },
         }
         
@@ -35,195 +35,42 @@ const appConfig = {
 async function getConfig() {
     return jsonify(appConfig)
 }
+
+
 async function getCards(ext) {
     ext = argsify(ext)
     let cards = []
+    // let page = ext.page
+    // let id = ext.id
     let { page = 1, id } = ext
-    const url =appConfig.site + `/list/?country=${id}&page=${page}` 
+    const url = appConfig.site + id
     const { data } = await $fetch.get(url, {
         headers: {
             'User-Agent': UA,
-            'Referer': 'https://yhdm.one/',
-            'Origin': 'https://yhdm.one',
         },
     })
     const $ = cheerio.load(data)
-    const videos = $('ul.list-unstyled li')
+    const videos = $('#dataList .public-list-box')
     videos.each((_, e) => {
-        const href = $(e).find('a').attr('href')
-        const title = $(e).find('h6').text()
-        const cover =appConfig.site + $(e).find('img').attr('data-original')
-        //const remarks = $(e).find('.note > span').text()
+        const href =  $(e).find('.public-list-exp').attr('href')
+        const img = $(e).find('img');  // 获取图片元素
+        const cover = img.attr('data-src') || img.attr('src');  // 获取封面图片链接，优先使用 data-src
+        
+        const title = img.attr('alt');  // 获取影片标题
+        
+    
+        //const remarks = 
         cards.push({
             vod_id: href,
             vod_name: title,
             vod_pic: cover,
-            //vod_remarks: remarks, // 海報右上角的子標題
-            ext: {
-                url: `${appConfig.site}${href}`,
-            },
-        })
-    })
-
-    return jsonify({
-        list: cards,
-    })
-}
-
-
-//
-//
-
-async function getTracks(ext) {
-    
-    ext = argsify(ext)
-   let groups = []
-    
-    let url = ext.url
-    
-    
-
-    const { data } = await $fetch.get(url, {
-        headers: {
-            'User-Agent': UA,
-        },
-    })
-
-
-    
-
-    const $ = cheerio.load(data)
-    const playlist = $('.ep-panel.mb-3 a')
-    for (const e of playlist) {
-       
-        let name = $(e).attr('title')
-        const regex = /\/vod-play\/(.*?)\.html/;
-
-        const ShareUrl = 'https://yhdm.one/_get_plays/' +  $(e).attr('href').match(regex)[1];
-
-
-        
-         let group = {
-              title:name ,
-              tracks: [],
-        }
-
-
-//后加
-        
-         const new_data = await $fetch.get(ShareUrl, {
-                headers: {
-                    'User-Agent': UA,
-                      'Referer': 'https://yhdm.one/',
-                        'Origin': 'https://yhdm.one',
-                },
-            });
-
-        const playlists = argsify(new_data.data).video_plays
-
-/*
-        
-
-     let  playlists = [
-            { "play_data": "https://hd.ijycnd.com/play/9b6589Na/index.m3u8", "src_site": "jyzy" },
-            { "play_data": "https://hn.bfvvs.com/play/Le351wpb/index.m3u8", "src_site": "hnzy" },
-            { "play_data": "https://play.xluuss.com/play/7e55yLXe/index.m3u8", "src_site": "xlzy" }
-        ];
-
-
-*/
-         for (const d of playlists) {
-            group.tracks.push({
-                name: d.src_site,
-                pan: '',
-                ext: {
-                    url: d.play_data,
-                },
-            });
-        }
-
-        
-     
-
-if (group.tracks.length > 0) {
-      groups.push(group)
-    }
-
-
-
-
-
-        
-    }
-
-
-
-
-
-    
-
-return jsonify({ list: groups })
-      
-   
-}
-
-
-
-
-
-
-
-async function getPlayinfo(ext) {
-    ext = argsify(ext)
-    const url = ext.url
-    return jsonify({ urls: [url] })
-}
-
-
-
-async function search(ext) {
-    ext = argsify(ext)
-    let cards = []
-
-    let text = encodeURIComponent(ext.text)
-    let page = ext.page || 1
-    let url = `${appConfig.site}/search?q=${text}`//https://yhdm.one/search?q=%E5%90%8D
-
-    const { data } = await $fetch.get(url, {
-        headers: {
-            'User-Agent': UA,
-        },
-    })
-
-    const $ = cheerio.load(data)
-
-    const videos = $('.list-unstyled li')
-    videos.each((_, e) => {
-
-        const link = $(li).find('a').first();
-        
-        const href = link.attr('href');
-
-        
-        const img = $(li).find('img').first(); 
-        
-        const cover = img.attr('data-original') || img.attr('src');
-        const title = img.attr('alt');
-
-
-
-        
-        cards.push({
-            vod_id: href,
-            vod_name: title,
-            vod_pic: appConfig.site + cover,
             vod_remarks: '',
-
             ext: {
                 url: `${appConfig.site}${href}`,
             },
         })
     })
+
     return jsonify({
         list: cards,
     })

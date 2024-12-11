76fdd8b6ced82ab13a3d131ecd16be4b928d00f7
kingreevice <129922512+kingreevice@users.noreply.github.com>
2024-12-11 14:17:06 +0800

    Update ceshi.js

--- js/ceshi.js
+++ js/ceshi.js
@@ -7,7 +7,7 @@ const appConfig = {
     site: 'https://www.j00j.com',
     tabs: [
         {
-            name: '欧美剧18',
+            name: '欧美剧88',
             ext: {
                 id: 20,
             },

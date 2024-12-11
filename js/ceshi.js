cb2251bd7b9b0204b1974a1defc15b2447a90f3a
kingreevice <129922512+kingreevice@users.noreply.github.com>
2024-12-11 17:24:02 +0800

    Update ceshi.js

--- js/ceshi.js
+++ js/ceshi.js
@@ -7,7 +7,7 @@ const appConfig = {
     site: 'https://www.j00j.com',
     tabs: [
         {
-            name: '欧美剧9894',
+            name: '欧美剧94',
             ext: {
                 id: 20,
             },
@@ -190,7 +190,7 @@ const New_url = 'https://www.j00j.com/index.php/vod/play/id/13858/sid/1/nid/1.ht
               
  
 const aa = JSON.stringify(other_data);
-const bb = aa.substring(0, 200)
+const bb = aa.substring(0, 20)
     // 
 
               

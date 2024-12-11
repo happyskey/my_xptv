1726555be6f96f5572e25e4afe64f09689fd69b8
kingreevice <129922512+kingreevice@users.noreply.github.com>
2024-12-11 21:50:53 +0800

    Update ceshi.js

--- js/ceshi.js
+++ js/ceshi.js
@@ -190,11 +190,11 @@ const New_url = appConfig.site + href
               
  
 const new_html =cheerio.load(other_data.data) //other_data.data// JSON.stringify(other_data.data);
-const url = new_html('#playleft iframe').attr('src')
- //const scriptContent = new_html('script:contains("player_aaaa")').text()
+//const url = new_html('#playleft iframe').attr('src')
+ const scriptContent = new_html('script:contains("player_aaaa")').text()
               
-//const Regex = /"url":"(.*?)"/;
-//const url = scriptContent.match(Regex)[1].replace(/\\/g, "")
+const Regex = /"url":"(.*?)"/;
+const url = scriptContent.match(Regex)[1].replace(/\\/g, "")
 
 
 

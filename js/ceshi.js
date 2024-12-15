edf41171d65d5f2b1b66043fce4d8ccf060bdcd8
kingreevice <129922512+kingreevice@users.noreply.github.com>
2024-12-16 07:48:30 +0800

    Update ceshi.js

--- js/ceshi.js
+++ js/ceshi.js
@@ -69,9 +69,12 @@ async function getCards(ext) {
     let { page = 1, id } = ext
 
     // 定義請求的 URL
+if (id ==='/movies' && page ===1){
+     const url = appConfig.site + `${id}`  
 
+}else{
  const url = appConfig.site + `${id}/page/${page}`  
-
+}
     // 使用內置的 http client 發起請求獲取 html
     const { data } = await $fetch.get(url, {
         headers: {

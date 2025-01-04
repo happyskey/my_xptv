f4dbfef9bf491fe422b6ccc90297a2308bfa7bc5
kingreevice <129922512+kingreevice@users.noreply.github.com>
2025-01-04 10:20:31 +0800

    Update GYING.js

--- js/GYING.js
+++ js/GYING.js
@@ -39,7 +39,7 @@ async function getCards(ext) {
     ext = argsify(ext)
     let cards = []
     let { page = 1, id } = ext
-    const url =appConfig.site + `/index.php/vod/show/id/${id}/page/${page}.html` 
+    const url =appConfig.site + ${id}+${page}
     const { data } = await $fetch.get(url, {
         headers: {
             'User-Agent': UA,

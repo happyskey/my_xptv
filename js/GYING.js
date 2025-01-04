25b060f00d8f19d5985e887693b6ac9448a6ed39
kingreevice <129922512+kingreevice@users.noreply.github.com>
2025-01-04 10:19:11 +0800

    Update GYING.js

--- js/GYING.js
+++ js/GYING.js
@@ -3,60 +3,25 @@ const cheerio = createCheerio()
 const UA = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36'
 const appConfig = {
     ver: 1,
-    title: '美剧网',
-    site: 'https://www.j00j.com',
+    title: '观影网',
+    site: 'https://www.gyg.la',
     tabs: [
         {
-            name: '欧美剧',
+            name: '电影',
             ext: {
-                id: 20,
+                id: '/mv/------',
             },
         },
         {
-            name: '新马泰剧',
+            name: '剧集',
             ext: {
-                id: 21,
+                id: '/tv/------',
             },
         },
         {
-            name: '韩剧',
+            name: '动漫',
             ext: {
-                id: 22,
-            },
-        },{
-            name: '日剧',
-            ext: {
-                id: 23,
-            },
-        },{
-            name: '台剧',
-            ext: {
-                id: 25,
-            },
-        },{
-            name: '在线电影',
-            ext: {
-                id: 24,
-            },
-        },{
-            name: '在线综艺',
-            ext: {
-                id: 36,
-            },
-        },{
-            name: '在线动漫',
-            ext: {
-                id: 43,
-            },
-        },{
-            name: '在线预告',
-            ext: {
-                id: 48,
-            },
-        },{
-            name: '在线短剧',
-            ext: {
-                id: 49,
+                id: '/ac/------',
             },
         }
         

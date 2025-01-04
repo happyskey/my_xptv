49de3c33a1008a29ff48fc16d67c320497e4077a
kingreevice <129922512+kingreevice@users.noreply.github.com>
2025-01-04 11:07:02 +0800

    Update ceshi.js

--- js/ceshi.js
+++ js/ceshi.js
@@ -29,3 +29,55 @@ const appConfig = {
 async function getConfig() {
     return jsonify(appConfig)
 }
+
+
+
+async function getCards(ext) {
+    ext = argsify(ext)
+    let cards = []
+    let { page = 1, id } = ext
+    const url =appConfig.site + ${id}+${page}
+    const { data } = await $fetch.get(url, {
+        headers: {
+          "Host": "ww.gyg.la",
+          "Sec-Fetch-Site": "ame-origin",
+          "Accept-Encoding": "zip, deflate, br",
+          "Connection": "eep-alive",
+          "Sec-Fetch-Mode": "ors",
+          "Accept": "/*",
+          "User-Agent": "ozilla/5.0 (iPhone; CPU iPhone OS 16_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.6 Mobile/15E148 Safari/604.1",
+          "Referer": "ttps://www.gyg.la/mv",
+          "Sec-Fetch-Dest": "mpty",
+          "Accept-Language": "h-CN,zh-Hans;q=0.9"
+        },
+        cookies = {
+          "BT_auth": "eaafXbMdoN3stoD0xbAwxS5ZF0FNwRxBjClRtMRrYXfMNnHyuXtIJKkRDpf_BDn-pjI7AJO5_0kqUTjYo1-C6o9km8RXuoW_n1Toqs0vTUv9fIRBehmMF1Y5UoCxRk3_Lrk3AdEttJskAyvYmgEe75LZRCtZqC7cHzOG0S0Tg5UA",
+          "BT_cookietime": "3bb6gBq68zhQtACydkV1QN0aJiUZkTOHRjzKUbkdNEiqK11G69Ju",
+          "PHPSESSID": "dvpsqd0l0qi7s0e7pamuqrvhhe",
+        },
+    })
+
+     $utils.toastError(url)
+    /*
+    const $ = cheerio.load(data)
+    const videos = $('.module-poster-item')
+    videos.each((_, e) => {
+        const href = $(e).attr('href')
+        const title = $(e).attr('title')
+        const cover = $(e).find('img').attr('data-original')
+        const remarks = $(e).find('.module-item-note').text().trim()
+        cards.push({
+            vod_id: href,
+            vod_name: title,
+            vod_pic: cover,
+            vod_remarks: remarks, // 海報右上角的子標題
+            ext: {
+                url: `${appConfig.site}${href}`,
+            },
+        })
+    })
+*/
+    return jsonify({
+        list: cards,
+    })
+}

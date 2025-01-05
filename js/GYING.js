bc3ce53cb31a644b6fc0445808c9baf71df4b2a9
kingreevice <129922512+kingreevice@users.noreply.github.com>
2025-01-05 08:12:40 +0800

    Update GYING.js

--- js/GYING.js
+++ js/GYING.js
@@ -1,6 +1,10 @@
 //昊
  const cheerio = createCheerio()
 const UA = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36'
+const cookie = ["BT_auth=eaafXbMdoN3stoD0xbAwxS5ZF0FNwRxBjClRtMRrYXfMNnHyuXtIJKkRDpf_BDn-pjI7AJO5_0kqUTjYo1-C6o9km8RXuoW_n1Toqs0vTUv9fIRBehmMF1Y5UoCxRk3_Lrk3AdEttJskAyvYmgEe75LZRCtZqC7cHzOG0S0Tg5UA",
+      "BT_cookietime=3bb6gBq68zhQtACydkV1QN0aJiUZkTOHRjzKUbkdNEiqK11G69Ju",
+      "PHPSESSID=dvpsqd0l0qi7s0e7pamuqrvhhe"
+    ].join("; ")
 const appConfig = {
     ver: 1,
     title: '观影网',
